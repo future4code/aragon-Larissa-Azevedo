4b8880759be756b9417025b30d2750f44f66c815
Larissa <larissadecastro18@gmail.com>
2022-05-05 22:30:46 -0300

    adicionei os arquivos iniciais para o projeto labefy

---
+++ modulo2/projeto-labefy/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+
+export default class App extends React.Component {
+  render() {
+
+  return (
+    <section>
+     
+    </section>
+  );
+}
+};
