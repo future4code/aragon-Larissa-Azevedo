54dd2491ada567388bd1c16cbb96495100a16b7c
Larissa <larissadecastro18@gmail.com>
2022-05-24 18:13:18 -0300

    corrigi linha de url que estava com erro

--- modulo3/projeto-labex/src/requests/services.js
+++ modulo3/projeto-labex/src/requests/services.js
@@ -7,7 +7,7 @@ export const requestLogin = (email, password, navigate) => {
         password: password
     }
 
-    axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labeX/:lari-azevedo-aragon/login", body)
+    axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labeX/lari-azevedo-aragon/login", body)
     .then((res) => {
         localStorage.setItem("token", res.data.token);
         alert("Acesso Autorizado!")
