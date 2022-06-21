357ed2177433a391cb60db63f042693f8b941d59
Larissa <larissadecastro18@gmail.com>
2022-06-21 11:26:41 -0300

    adicionei a resolução parcial do exercício 2

---
+++ modulo4/dia2/exercicio2.js
@@ -0,0 +1,15 @@
+let geraTabuada = (multiplicador) => {
+  let multiplica = multiplicador;
+  let i;
+  console.log(`Tabuada de ${multiplicador}`);
+
+  if (multiplica > 10 || multiplica === 0) {
+    console.log(`Erro. Parâmetro inválido. Número precisa valer entre 1 e 10.`);
+  } else {
+    for (i = 1; i <= 10; i++) {
+      console.log(multiplica + " X " + i + " = " + multiplica * i);
+    }
+  }
+};
+
+geraTabuada(0);
