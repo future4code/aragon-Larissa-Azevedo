8e73438d7de4099062956d51937a5a10c0c41a0f
Larissa <larissadecastro18@gmail.com>
2022-06-23 13:25:29 -0300

    adicionei mais uma resolução parcial, não consegui evitar repetição de nomes

--- modulo4/revisão-js/dia4/exercicio1.js
+++ modulo4/revisão-js/dia4/exercicio1.js
@@ -16,12 +16,48 @@ const segundaLista= [
 	}
 ]
 
+// function geraItensUnicos() {
+//     const primeiraLista = [
+//         {
+//             nome: "Banana"
+//         },
+//         {
+//             nome: "Laranja"
+//         }
+//     ]
+    
+//     const segundaLista= [
+//         {
+//             nome: "Laranja"
+//         },
+//         {
+//             nome: "Cebola"
+//         }
+//     ]
+
+//     const listaUnica = []
+
+//     if(primeiraLista.nome === segundaLista.nome){
+//         return listaUnica.push(primeiraLista, segundaLista)
+//     }
+    
+// }
+
 
 function geraItensUnicos(primeiraLista, segundaLista) {
     const listaUnificada = primeiraLista.concat(segundaLista);
     for (let i=0; i < listaUnificada.length; i++){
     return listaUnificada}
 
+
+    let index = listaUnificada.findIndex((value)=>value.nome === value.nome)
+    if (index < 0) {return listaUnificada}
+
 }      
 
-    console.log(geraItensUnicos(primeiraLista, segundaLista))
+// let checaPrimeiraLista = primeiraLista.findIndex((value)=>value.nome === value.nome)
+//     let checaSegundaLista = segundaLista.findIndex((value)=>value.nome === value.nome)
+//     if (checaPrimeiraLista === checaSegundaLista) {return primeiraLista.concat(segundaLista);}
+
+console.log(geraItensUnicos(primeiraLista, segundaLista))
+
