d9a227d5537a00a9dcdaca6dac074d4a844443c2
Larissa <larissadecastro18@gmail.com>
2022-06-23 16:29:29 -0300

    reescrevi parte do código mas ainda tive dificuldades - exercício incomplet

--- modulo4/revisão-js/dia4/exercicio1.js
+++ modulo4/revisão-js/dia4/exercicio1.js
@@ -1,37 +1,33 @@
 const primeiraLista = [
-	{
-		nome: "Banana"
-	},
-	{
-		nome: "Laranja"
-	}
-]
+  {
+    nome: "Banana",
+  },
+  {
+    nome: "Laranja",
+  },
+];
 
-const segundaLista= [
-	{
-		nome: "Laranja"
-	},
-	{
-		nome: "Cebola"
-	}
-]
-
+const segundaLista = [
+  {
+    nome: "Laranja",
+  },
+  {
+    nome: "Cebola",
+  },
+];
 
 function geraItensUnicos(primeiraLista, segundaLista) {
-    const listaUnificada = primeiraLista.concat(segundaLista);
-    const listaFinal = [];
+  const listaUnificada = primeiraLista.concat(segundaLista);
+  const listaFinal = [];
 
-    console.log(listaUnificada.nome) //não consigo acessar o nome, retorna undefined
-    
-    for (let i=0; i < listaUnificada.length; i++){
-        if(listaUnificada.find(element => element === listaUnificada.nome)){ //se a lista unificada encontrar nomes repetidos não deve inclui-lo na listaFinal
-            //pesquisei métodos de pesquisa e outros para evitar duplicação mas tive dificuldades para aplicar aqui.
-            listaFinal.push(listaUnificada)
-        } 
+  for (let itemLista of listaUnificada) {
+    const itemFinal = listaFinal.find((item) => item.nome === itemLista.nome);
+
+    if (!itemFinal) {
+      listaFinal.push(itemLista);
     }
-    return listaFinal
-
+  }
+  return listaFinal;
 }
 
-console.log(geraItensUnicos(primeiraLista, segundaLista))
-
+console.log(geraItensUnicos(primeiraLista, segundaLista));
