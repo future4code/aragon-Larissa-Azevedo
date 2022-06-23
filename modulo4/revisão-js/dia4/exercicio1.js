01b461d3d38b0934c3c7ae3eacecd58745da7479
Larissa <larissadecastro18@gmail.com>
2022-06-23 10:05:44 -0300

    adicionei o  arquivo inicial para o exercício do dia 4  de Revisão JS

---
+++ modulo4/revisão-js/dia4/exercicio1.js
@@ -0,0 +1,25 @@
+const primeiraLista = [
+	{
+		nome: "Banana"
+	},
+	{
+		nome: "Laranja"
+	}
+]
+
+const segundaLista= [
+	{
+		nome: "Laranja"
+	},
+	{
+		nome: "Cebola"
+	}
+]
+
+function geraItensUnicos(primeiraLista, segundaLista, index) {
+    return 
+
+
+    
+}
+
