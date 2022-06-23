24e5a20ab36a85343a6455021a83155e1c0cb7d8
Larissa <larissadecastro18@gmail.com>
2022-06-23 15:42:09 -0300

    reescrevi parte do código mas ainda tive dificuldades - exercício incompleto

--- modulo4/revisão-js/dia4/exercicio1.js
+++ modulo4/revisão-js/dia4/exercicio1.js
@@ -16,48 +16,20 @@ const segundaLista= [
 	}
 ]
 
-// function geraItensUnicos() {
-//     const primeiraLista = [
-//         {
-//             nome: "Banana"
-//         },
-//         {
-//             nome: "Laranja"
-//         }
-//     ]
-    
-//     const segundaLista= [
-//         {
-//             nome: "Laranja"
-//         },
-//         {
-//             nome: "Cebola"
-//         }
-//     ]
-
-//     const listaUnica = []
-
-//     if(primeiraLista.nome === segundaLista.nome){
-//         return listaUnica.push(primeiraLista, segundaLista)
-//     }
-    
-// }
-
 
 function geraItensUnicos(primeiraLista, segundaLista) {
     const listaUnificada = primeiraLista.concat(segundaLista);
+    const listaFinal = [];
+    
     for (let i=0; i < listaUnificada.length; i++){
-    return listaUnificada}
+        if(listaUnificada ){ //se a lista unificada encontrar nomes repetidos não deve inclui-lo na listaFinal
+            //pesquisei métodos de pesquisa mas tive dificuldades para aplicar aqui.
 
+        } listaFinal.push(listaUnificada)
+    }
+    return listaFinal
 
-    let index = listaUnificada.findIndex((value)=>value.nome === value.nome)
-    if (index < 0) {return listaUnificada}
-
-}      
-
-// let checaPrimeiraLista = primeiraLista.findIndex((value)=>value.nome === value.nome)
-//     let checaSegundaLista = segundaLista.findIndex((value)=>value.nome === value.nome)
-//     if (checaPrimeiraLista === checaSegundaLista) {return primeiraLista.concat(segundaLista);}
+}
 
 console.log(geraItensUnicos(primeiraLista, segundaLista))
 
