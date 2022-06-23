5d60324d631659bc1b4a2999ac2c5a6116acb59e
Larissa <larissadecastro18@gmail.com>
2022-06-23 15:58:35 -0300

    reescrevi parte do código mas ainda tive dificuldades - exercício incompleto

--- modulo4/revisão-js/dia4/exercicio1.js
+++ modulo4/revisão-js/dia4/exercicio1.js
@@ -20,12 +20,14 @@ const segundaLista= [
 function geraItensUnicos(primeiraLista, segundaLista) {
     const listaUnificada = primeiraLista.concat(segundaLista);
     const listaFinal = [];
+
+    console.log(listaUnificada.nome) //não consigo acessar o nome, retorna undefined
     
     for (let i=0; i < listaUnificada.length; i++){
-        if(listaUnificada ){ //se a lista unificada encontrar nomes repetidos não deve inclui-lo na listaFinal
-            //pesquisei métodos de pesquisa mas tive dificuldades para aplicar aqui.
-
-        } listaFinal.push(listaUnificada)
+        if(listaUnificada.find(element => element === listaUnificada.nome)){ //se a lista unificada encontrar nomes repetidos não deve inclui-lo na listaFinal
+            //pesquisei métodos de pesquisa e outros para evitar duplicação mas tive dificuldades para aplicar aqui.
+            listaFinal.push(listaUnificada)
+        } 
     }
     return listaFinal
 
