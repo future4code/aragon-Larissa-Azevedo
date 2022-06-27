99d840b45987ba90bd9c659318ac14d96091bdda
Larissa <larissadecastro18@gmail.com>
2022-06-27 16:54:49 -0300

    adicionando exercício 3

--- modulo4/node-package-json/index.js
+++ modulo4/node-package-json/index.js
@@ -15,25 +15,35 @@
 
 //_Exercício 2_
 
-let operacoes = process.argv[2];
+// let operacoes = process.argv[2];
 
-switch (operacoes) {
-    case "add":
-        console.log(Number(process.argv[3]) + Number(process.argv[4]))
-    break;
+// switch (operacoes) {
+//     case "add":
+//         console.log(Number(process.argv[3]) + Number(process.argv[4]))
+//     break;
 
-    case "sub":
-        console.log(Number(process.argv[3]) - Number(process.argv[4]))
-    break;
+//     case "sub":
+//         console.log(Number(process.argv[3]) - Number(process.argv[4]))
+//     break;
 
-    case "mult":
-        console.log(Number(process.argv[3]) * Number(process.argv[4]))
-    break;
+//     case "mult":
+//         console.log(Number(process.argv[3]) * Number(process.argv[4]))
+//     break;
 
-    case "div":
-        console.log(Number(process.argv[3]) / Number(process.argv[4]))
-    break;    
-}
-
+//     case "div":
+//         console.log(Number(process.argv[3]) / Number(process.argv[4]))
+//     break;    
+// }
 
 //_Exercício 3_
+
+let tarefas = "Lavar a Louça";
+
+const adicionaTarefa = (tarefa) => {
+    listaAtualizada = [tarefas, tarefa]
+    console.log("Nova tarefa adicionada :)")
+
+    return listaAtualizada
+}
+
+console.log(adicionaTarefa(process.argv[2]))
