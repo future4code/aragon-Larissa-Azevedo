d739ef3890b4cddef2c2c654f4b9f8bf5289d09c
Larissa <larissadecastro18@gmail.com>
2022-06-27 16:22:33 -0300

    adicionando exercício 2

---
+++ modulo4/node-package-json/index.js
@@ -0,0 +1,39 @@
+// _Exercício 1_
+
+//1.a) Acessamos os parametros passados na linha de comando através de "process.argv[posicao-do-parametro]"
+
+//1.b)
+// const nome =  process.argv[2] 
+// const idade = process.argv[3]
+// console.log(`Olá, ${nome}! Você tem ${idade} anos!`)
+
+//1.c)
+// const nome =  process.argv[2] 
+// const idade = process.argv[3]
+// const somaIdade = Number(idade) + 7
+// console.log(`Olá, ${nome}! Você tem ${idade} anos! Daqui a sete anos você terá ${somaIdade} anos.`)
+
+//_Exercício 2_
+
+let operacoes = process.argv[2];
+
+switch (operacoes) {
+    case "add":
+        console.log(Number(process.argv[3]) + Number(process.argv[4]))
+    break;
+
+    case "sub":
+        console.log(Number(process.argv[3]) - Number(process.argv[4]))
+    break;
+
+    case "mult":
+        console.log(Number(process.argv[3]) * Number(process.argv[4]))
+    break;
+
+    case "div":
+        console.log(Number(process.argv[3]) / Number(process.argv[4]))
+    break;    
+}
+
+
+//_Exercício 3_
