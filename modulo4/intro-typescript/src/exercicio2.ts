554fcefdaac586373cfdc98d79d1e92cbb5d6bb6
Larissa <larissadecastro18@gmail.com>
2022-06-28 15:15:11 -0300

    adicionando exercício 2

---
+++ modulo4/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,10 @@
+
+const coresFavoritas: string[] = [process.argv[2], process.argv[3], process.argv[4]]
+
+function imprimeTresCoresFavoritas(coresFavoritas:string[]) {
+   
+    console.log(`Suas cores favoritas são ${coresFavoritas}.`)
+
+ }
+ 
+ imprimeTresCoresFavoritas(coresFavoritas)
