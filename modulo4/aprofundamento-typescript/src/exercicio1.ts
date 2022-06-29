7c5c74c84bcd03620569bea4a79076093c0f82e6
Larissa <larissadecastro18@gmail.com>
2022-06-29 11:04:41 -0300

    adicionando exercicio1 e scripts

---
+++ modulo4/aprofundamento-typescript/src/exercicio1.ts
@@ -0,0 +1,9 @@
+//_Exercicio 1_
+
+// const ano:string = "2022" 
+const ano:string | number = 2022
+
+//Ao reatribuir um número a uma variável do tipo string é mostrado um erro 
+// apontando que "o tipo 'number' não pode ser atribuído ao tipo 'string'""
+//PAra que a variável "ano" também receba o tipo 'number' E 'string' é necessária  tipagem 'Union Types', com o símbolo pipeline |
+
