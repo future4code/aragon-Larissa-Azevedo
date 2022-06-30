20373adffce57ededad6da26dbd9dd35e39f6ea2
Larissa <larissadecastro18@gmail.com>
2022-06-30 16:59:43 -0300

    adicionando exercicio5 e alterações no exercicio4

--- modulo4/projeto-lista-typescript/src/exercicio4.ts
+++ modulo4/projeto-lista-typescript/src/exercicio4.ts
@@ -5,20 +5,20 @@ type Pessoas = {
   presencial: boolean;
 };
 
-enum setores {
+enum Setores {
   MARKETING = "Marketing",
   VENDAS = "Vendas",
   FINANCEIRO = "Financeiro",
 }
 
 const pessoasColaboradoras: Pessoas[] = [
-  { nome: "Marcos", salário: 2500, setor: setores.MARKETING, presencial: true },
-  { nome: "Maria", salário: 1500, setor: setores.VENDAS, presencial: false },
-  {nome: "Salete", salário: 2200, setor: setores.FINANCEIRO,presencial: true,},
-  { nome: "João", salário: 2800, setor: setores.MARKETING, presencial: false },
-  { nome: "Josué", salário: 5500, setor: setores.FINANCEIRO, presencial: true },
-  { nome: "Natalia", salário: 4700, setor: setores.VENDAS, presencial: true },
-  { nome: "Paola", salário: 3500, setor: setores.MARKETING, presencial: true },
+  { nome: "Marcos", salário: 2500, setor: Setores.MARKETING, presencial: true },
+  { nome: "Maria", salário: 1500, setor: Setores.VENDAS, presencial: false },
+  {nome: "Salete", salário: 2200, setor: Setores.FINANCEIRO,presencial: true,},
+  { nome: "João", salário: 2800, setor: Setores.MARKETING, presencial: false },
+  { nome: "Josué", salário: 5500, setor: Setores.FINANCEIRO, presencial: true },
+  { nome: "Natalia", salário: 4700, setor: Setores.VENDAS, presencial: true },
+  { nome: "Paola", salário: 3500, setor: Setores.MARKETING, presencial: true },
 ];
 
 function marketingPresencial(pessoasColaboradoras: Pessoas[]): Pessoas[] {
