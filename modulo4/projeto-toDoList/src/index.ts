b03ca203824330004916adc5a3f3ed3fe80d174d
Larissa <larissadecastro18@gmail.com>
2022-07-14 10:49:55 -0300

    adicionando os arquivos iniciais para o projeto

---
+++ modulo4/projeto-toDoList/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import { ping } from "./endpoints/ping";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT || 3003, () => {
+  console.log(`Servidor rodando na porta ${process.env.PORT || 3003}`)
+});
+
+// Endpoint com o callback vindo por import da pasta endpoints
+app.get("/ping", ping)
+
+// Siga o exemplo do ping acima e monte seus endpoints abaixo!
