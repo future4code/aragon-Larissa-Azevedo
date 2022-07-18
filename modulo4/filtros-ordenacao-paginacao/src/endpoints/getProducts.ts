8f56c2bff3a5dd66147a5f7d59b3a9e7926a624d
Larissa <larissadecastro18@gmail.com>
2022-07-18 15:50:26 -0300

    adicionando resolução do exercicio2

--- modulo4/filtros-ordenacao-paginacao/src/endpoints/getProducts.ts
+++ modulo4/filtros-ordenacao-paginacao/src/endpoints/getProducts.ts
@@ -1,37 +1,38 @@
-//_Exercícios 1_
+//_Exercícios 1_e_2_
 import { Request, Response } from "express";
 import connection from "../database/connection";
 import { TABLE_PRODUCTS } from "../database/tableNames";
 
-export const getProducts = async (req:Request, res:Response) =>{
-    let errorCode = 400
+export const getProducts = async (req: Request, res: Response) => {
+  let errorCode = 400;
 
-    try {
-        const query = req.query.q
-        const sort = req.query.sort || "price"
-        const order = req.query.order || "asc"
+  try {
+    const query = req.query.q;
+    const sort = req.query.sort || "id";
+    const order = req.query.order || "asc";
+    const limit = Number(req.query.limit) || 10;
+    const page = Number(req.query.page) || 1;
+    const offset = limit * (page - 1);
 
-        if(query){
-            const [result] = await connection.raw(`
+    if (query) {
+      const [result] = await connection.raw(`
             SELECT * FROM ${TABLE_PRODUCTS}
             WHERE name LIKE "%${query}%"
-            ORDER BY ${sort} ${order};`)
+            ORDER BY ${sort} ${order}
+            LIMIT ${limit}
+            OFFSET ${offset} ;`);
 
-            return res.status(200).send({products: result})
-        }
-
-
-        const [ result ] = await connection.raw(`
-        SELECT * FROM ${TABLE_PRODUCTS}
-        ORDER BY ${sort} ${order} ;`)
-
-        return res.status(200).send({products: result})
-
-
-    } catch (error) {
-        res.status(errorCode).send({ message: error.message })
+      return res.status(200).send({ products: result });
     }
 
-}
+    const [result] = await connection.raw(`
+        SELECT * FROM ${TABLE_PRODUCTS}
+        ORDER BY ${sort} ${order}
+        LIMIT ${limit}
+        OFFSET ${offset} ;`);
 
-
+    return res.status(200).send({ products: result });
+  } catch (error) {
+    res.status(errorCode).send({ message: error.message });
+  }
+};
