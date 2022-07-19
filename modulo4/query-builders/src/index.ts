f3a1ec2921db43a57955d7b9964e22067abc29e1
Larissa <larissadecastro18@gmail.com>
2022-07-19 13:58:22 -0300

    adicionando arquivos iniciais para os exercícios

---
+++ modulo4/query-builders/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import { ping } from "./endpoints/ping";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT || 3003, () => {
+  console.log(`Servidor rodando na porta ${process.env.PORT || 3003}`)
+});
+
+app.get("/ping", ping)
+
+// Implemente seus endpoints abaixo
