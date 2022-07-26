626439aafeb60936d98cf84c94f3c23ad74ee269
Larissa <larissadecastro18@gmail.com>
2022-07-26 16:00:24 -0300

    adicionando resolucao do exercicio3

--- modulo5/heranca/src/database/UserDatabase.ts
+++ modulo5/heranca/src/database/UserDatabase.ts
@@ -1,4 +1,3 @@
-import { userInfo } from "os";
 import { User } from "../models/User";
 import { BaseDatabase } from "./BaseDatabase";
 
--- modulo5/heranca/src/endpoints/createUser.ts
+++ modulo5/heranca/src/endpoints/createUser.ts
@@ -1,38 +1,26 @@
-import { Request, Response } from "express"
-import connection from "../database/connection"
-import { TABLE_USERS } from "../database/tableNames"
-import { User } from "../models/User"
+import { Request, Response } from "express";
+import connection from "../database/connection";
+import { TABLE_USERS } from "../database/tableNames";
+import { UserDatabase } from "../database/UserDatabase";
+import { User } from "../models/User";
 
 export const createUser = async (req: Request, res: Response) => {
-    let errorCode = 400
-    try {
-        const email = req.body.email
-        const password = req.body.password
+  let errorCode = 400;
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
 
-        if (!email || !password) {
-            throw new Error("Body inválido.")
-        }
-
-        // const newUser: User = {
-        //     id: Date.now().toString(),
-        //     email,
-        //     password
-        // }
-
-        const user = new User(
-            Date.now().toString(),
-            email,
-            password
-        )
-
-        await connection(TABLE_USERS).insert({
-            id: user.getId(),
-            email: user.getEmail(),
-            password: user.getPassword()
-        })
-        
-        res.status(201).send({ message: "Usuário criado", user: user })
-    } catch (error) {
-        res.status(errorCode).send({ message: error.message })
+    if (!email || !password) {
+      throw new Error("Body inválido.");
     }
-}
+
+    const user = new User(Date.now().toString(), email, password);
+
+    const userDatabase = new UserDatabase();
+    await userDatabase.createUser(user);
+
+    res.status(201).send({ message: "Usuário criado", user: user });
+  } catch (error) {
+    res.status(errorCode).send({ message: error.message });
+  }
+};
