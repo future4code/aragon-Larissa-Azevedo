a99073ee744101f19b2da66c101a79b4284da84b
Larissa <larissadecastro18@gmail.com>
2022-08-15 14:22:35 -0300

    adicionando template para os exercícios do dia

---
+++ modulo5/testes-jest/tests/UserBusiness/signup.test.ts
@@ -0,0 +1,17 @@
+import { UserBusiness } from "../../src/business/UserBusiness"
+import { ISignupInputDTO } from "../../src/models/User"
+import { AuthenticatorMock } from "../mocks/services/AuthenticatorMock"
+import { HashManagerMock } from "../mocks/services/HashManagerMock"
+import { IdGeneratorMock } from "../mocks/services/IdGeneratorMock"
+import { UserDatabaseMock } from "../mocks/UserDatabaseMock"
+
+describe("Testando UserBusiness", () => {
+    const userBusiness = new UserBusiness(
+        new UserDatabaseMock(),
+        new IdGeneratorMock(),
+        new HashManagerMock(),
+        new AuthenticatorMock()
+    )
+
+    // implemente seu teste aqui
+})
