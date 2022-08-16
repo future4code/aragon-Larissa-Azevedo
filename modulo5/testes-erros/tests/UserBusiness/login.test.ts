413b61a39a05258f3afa7019205d1bd4a2ca970d
Larissa <larissadecastro18@gmail.com>
2022-08-16 17:09:51 -0300

    completando os testes de erros de login

--- modulo5/testes-erros/tests/UserBusiness/login.test.ts
+++ modulo5/testes-erros/tests/UserBusiness/login.test.ts
@@ -26,22 +26,78 @@ describe("Testando UserBusiness", () => {
         expect(response.token).toEqual("token-astrodev")
     })
 
-    test("deve retornar erro caso a senha seja inválida", async () => {
+    test("deve retornar erro caso email seja string vazia", async () => {
         expect.assertions(2)
 
         try {
             const input:ILoginInputDTO = {
-                email:"astrodev@gmail.com",
-                password:"bnaninha"
+                email:"",
+                password:"alice99"
+            }
+
+            await userBusiness.login(input)
+
+        } catch (error:unknown) {
+            if(error instanceof BaseError){
+                expect(error.statusCode).toEqual(400)
+                expect(error.message).toEqual("Parâmetro 'email' inválido")
+            }
+        }
+    })
+
+    test("deve retornar erro caso password não seja string", async () => {
+        expect.assertions(2)
+
+        try {
+            const input = {
+                email:"alice@gmail.com",
+                password:123
+            } as unknown as ILoginInputDTO
+
+            await userBusiness.login(input)
+
+        } catch (error:unknown) {
+            if(error instanceof BaseError){
+                expect(error.statusCode).toEqual(400)
+                expect(error.message).toEqual("Parâmetro 'password' inválido")
+            }
+        }
+    })
+
+    test("deve retornar erro caso password não menor que 6 caracteres", async () => {
+        expect.assertions(2)
+
+        try {
+            const input:ILoginInputDTO = {
+                email:"alice@gmail.com",
+                password:"ecila"
+            } 
+
+            await userBusiness.login(input)
+
+        } catch (error:unknown) {
+            if(error instanceof BaseError){
+                expect(error.statusCode).toEqual(400)
+                expect(error.message).toEqual("Parâmetro 'password' inválido: mínimo de 6 caracteres")
+            }
+        }
+    })    
+
+    test("deve retornar erro caso o email seja inválido",async () => {
+        expect.assertions(2)
+
+        try {
+            const input:ILoginInputDTO = {
+                email:"astrodevgmail.com",
+                password:"bananinha"
             }
             await userBusiness.login(input)
             
         } catch (error:unknown) {
             if(error instanceof BaseError){
-                expect(error.statusCode).toEqual(401)
-                expect(error.message).toEqual("Password incorreto")
-            }
-            
+                expect(error.statusCode).toEqual(400)
+                expect(error.message).toEqual("Parâmetro 'email' inválido")
+            }            
         }
     })
 
@@ -63,22 +119,23 @@ describe("Testando UserBusiness", () => {
             }
         }
     })
-
-    test("deve retornar erro caso o email seja inválido",async () => {
+    
+    test("deve retornar erro caso a senha seja incorreta", async () => {
         expect.assertions(2)
 
         try {
             const input:ILoginInputDTO = {
-                email:"astrodevgmail.com",
-                password:"bananinha"
+                email:"astrodev@gmail.com",
+                password:"bnaninha"
             }
             await userBusiness.login(input)
             
         } catch (error:unknown) {
             if(error instanceof BaseError){
-                expect(error.statusCode).toEqual(400)
-                expect(error.message).toEqual("Parâmetro 'email' inválido")
-            }            
+                expect(error.statusCode).toEqual(401)
+                expect(error.message).toEqual("Password incorreto")
+            }
+            
         }
     })
 })
