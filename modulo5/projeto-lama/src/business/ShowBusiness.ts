bc5d615aca946061cf50dac6934ed1ca3dc45501
Larissa <larissadecastro18@gmail.com>
2022-08-17 11:23:02 -0300

    adicionando template para o projeto da semana

---
+++ modulo5/projeto-lama/src/business/ShowBusiness.ts
@@ -0,0 +1,14 @@
+import { ShowDatabase } from "../database/ShowDatabase"
+import { Authenticator } from "../services/Authenticator"
+import { HashManager } from "../services/HashManager"
+import { IdGenerator } from "../services/IdGenerator"
+
+export class ShowBusiness {
+    constructor(
+        private showDatabase: ShowDatabase,
+        private idGenerator: IdGenerator,
+        private hashManager: HashManager,
+        private authenticator: Authenticator
+    ) {}
+
+}
---
+++ modulo5/projeto-lama/src/controller/UserController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { UserBusiness } from "../business/UserBusiness";
+
+export class UserController {
+    constructor(
+        private userBusiness: UserBusiness
+    ) { }
+
+   
+}
