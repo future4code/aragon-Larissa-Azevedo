987f90b604332fe950411891c61a755d35127233
Larissa <larissadecastro18@gmail.com>
2022-08-23 10:49:31 -0300

    adicionando endpoints de signup e login

--- modulo6/rodada-cases-1/src/index.ts
+++ modulo6/rodada-cases-1/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import { pingRouter } from './router/pingRouter';
+import { userRouter } from './router/userRouter';
 
 dotenv.config()
 const app = express()
@@ -15,3 +16,4 @@ app.listen(process.env.PORT || 3003, () => {
 })
 
 app.use("/ping", pingRouter)
+app.use("/users", userRouter)
--- modulo6/rodada-cases-1/src/services/Authenticator.ts
+++ modulo6/rodada-cases-1/src/services/Authenticator.ts
@@ -1,11 +1,13 @@
 import jwt from 'jsonwebtoken'
 import dotenv from "dotenv"
+import { USER_ROLES } from '../models/User'
 
 
 dotenv.config()
 
 export interface ITokenPayload {
-    id: string
+    id: string,
+    role:USER_ROLES
 }
 
 export class Authenticator {
